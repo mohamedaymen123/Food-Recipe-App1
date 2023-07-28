d73b4861a373453653c25881802b675ca0f12d80
mohamedaymen123 <guedouarmohamedaymen@gmail.com>
2023-07-28 19:30:02 +0200

    New project to push to GitHub

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Meal from './components/Meal';
+
+function App() {
+  return (
+    <div>
+      <Meal/>
+    </div>
+  );
+}
+
+export default App;
